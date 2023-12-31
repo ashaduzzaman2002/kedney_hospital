76cf5d5bb9a6951335c721471a687eb3ec2d0f27
ProgrammerAlok <alokpaul5584@gmail.com>
2023-12-31 18:37:58 +0530

    About page gallery component responsive design added

--- src/components/Gallery/AboutGallery.jsx
+++ src/components/Gallery/AboutGallery.jsx
@@ -8,22 +8,22 @@ const Data = [
     {
         image: Branch1,
         title: 'Branch 1 : Lal Darwaja',
-        position: 'justify-start',
+        position: 'justify-start mt-20 md:mt-0 ml-[10%] md:ml-0',
     },
     {
         image: Branch2,
         title: 'Branch 2 : Udhna Darwaja',
-        position: 'justify-end',
+        position: 'justify-end mr-[10%] md:mr-0',
     },
     {
         image: Branch3,
         title: 'Branch 3 : Adajan',
-        position: 'justify-start',
+        position: 'justify-start mr-[10%] md:mr-0',
     },
     {
         image: Branch4,
         title: 'Branch 4 : Amroli',
-        position: 'justify-end',
+        position: 'justify-end mt-[4rem] md:mt-0',
     },
 ];
 
@@ -46,17 +46,15 @@ const Card1 = ({ image, title, position }) => {
 const Card2 = ({ position }) => {
 
     return (
-        <div className={`${position} bg-gradient8 rounded-[1rem] w-[21.375rem] h-[20.875rem] overflow-hidden`}>
-
-        </div>
+        <div className={`${position} bg-gradient8 rounded-[1rem] w-[21.375rem] h-[20.875rem] overflow-hidden`}></div>
     );
 }
 
 const AboutGallery = () => {
   return (
-    <div className="py-[6rem]">
+    <div className="py-[6rem] overflow-x-hidden">
         <div className="relative">
-            <div className="mb-10">
+            <div className="md:mb-10">
                 <Heading1
                     subHeading={`Gallery`}
                     headingStart={`See Our `}
@@ -64,34 +62,15 @@ const AboutGallery = () => {
                     position="text-center"
                 />
             </div>
-            <div className="">
-                <div className="max-w-[90%] mx-auto grid grid-cols-4 gap-8 h-[36rem]">
+            <div className=" overflow-x-hidden">
+                <div className="max-w-[90%] mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 md:h-[36rem] overflow-hidden">
                     {Data.map((e, ind) => (
                         <Card1 key={ind} {...e} />
                     ))}
-
-
-
-
-                    {/* <div className="flex flex-col gap-2 mx-auto items-end justify-end">
-                        <img className="aspect-2/3 rounded-[1rem] overflow-hidden" src={Branch2} alt="branch-image-2" />
-                        <h3 className="w-full text-center font-fredoka font-[500] text-[1.5rem] text-[#000]">Branch 2 : Udhna Darwaja</h3>
-                    </div>
-                    <div className="flex flex-col gap-2 mx-auto items-start">
-                        <img className="aspect-2/3 rounded-[1rem] overflow-hidden" src={Branch3} alt="branch-image-3" />
-                        <h3 className="w-full text-center font-fredoka font-[500] text-[1.5rem] text-[#000]">Branch 3 : Adajan</h3>
-                    </div>
-                    <div className="flex flex-col gap-2 mx-auto items-end justify-end">
-                        <img className="aspect-2/3 rounded-[1rem] overflow-hidden" src={Branch4} alt="branch-image-4" />
-                        <h3 className="w-full text-center font-fredoka font-[500] text-[1.5rem] text-[#000]">Branch 4 : Amroli</h3>
-                    </div> */}
                 </div>
 
-                <div className="absolute">
-                    
-                </div>
-                <Card2 position={`absolute top-[10%] -left-[2%] -z-10`} />
-                <Card2 position={`absolute top-[35%] -right-[2%] -z-10`} />
+                <Card2 position={`absolute top-[6%] md:top-[10%] -left-[10%] -z-10`} />
+                <Card2 position={`absolute bottom-[5%] md:bottom-[15%] -right-[10%] -z-10`} />
                 
             </div>
         </div>
